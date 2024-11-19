2aa136d880143099d0cc4e5390cb17ab5146f5c7
Minh Hiếu <hieudm05@gmail.com>
2024-11-19 15:31:15 +0700

    lại

--- views/Clients/UpdateTaiKhoan/updateTk.php
+++ views/Clients/UpdateTaiKhoan/updateTk.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="vi">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Form Sửa Thông Tin Tài Khoản</title>
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body class="bg-light">
+
+<div class="bg-light">
     <div class="container my-5">
         <div class="row justify-content-center">
             <div class="col-lg-8">
@@ -56,8 +48,4 @@
             </div>
         </div>
     </div>
-
-    <!-- Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
+</div>
