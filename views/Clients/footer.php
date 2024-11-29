393d3262b154217f88f4f595a7dc4989e154bea7
luongne205 <doducluong2005@gmail.com>
2024-11-29 17:04:44 +0700

    fotter

--- views/Clients/footer.php
+++ views/Clients/footer.php
@@ -10,13 +10,13 @@
     <div class="row">
       <!-- Column 1 -->
       <div class="col-lg-3 col-md-6 mb-4">
-        <h5 class="text-white">Nhà sách NetaBooks Online</h5>
+        <h5 class="text-white">Nhà sách BARNES & NOBLE</h5>
         <p class="text-white">(Hoạt động từ 8h - 17h, thứ 2 - thứ 7)</p>
         <p class="text-white">
           <i class="fas fa-phone-alt"></i> Gọi đặt hàng: 028 7300 7684
         </p>
         <p class="text-white">
-          <i class="fas fa-envelope"></i> Email: hotro@netabooks.vn
+          <i class="fas fa-envelope"></i> Email: hotro@barnesnoble.vn
         </p>
       </div>
       <!-- Column 2 -->
@@ -34,9 +34,9 @@
       </div>
       <!-- Column 3 -->
       <div class="col-lg-3 col-md-6 mb-4">
-        <h5 class="text-white">Về NetaBooks</h5>
+        <h5 class="text-white">Về BARNES & NOBLE</h5>
         <ul class="list-unstyled">
-          <li><a href="#" class="text-white text-decoration-none">Giới thiệu NetaBooks</a></li>
+          <li><a href="#" class="text-white text-decoration-none">Giới thiệu BARNES & NOBLE</a></li>
           <li><a href="#" class="text-white text-decoration-none">Tuyển dụng</a></li>
         </ul>
         <h5 class="text-white mt-4">Sản phẩm khác</h5>
@@ -48,10 +48,10 @@
       </div>
       <!-- Column 4 -->
       <div class="col-lg-3 col-md-6 mb-4">
-        <h5 class="text-white">Kết nối với NetaBooks</h5>
+        <h5 class="text-white">Kết nối với BARNES & NOBLE</h5>
         <p><i class="fab fa-facebook"></i> 6.072 theo dõi trang</p>
         <p><i class="fab fa-instagram"></i> 355 theo dõi trang</p>
-        <h5 class="text-white mt-4">Gian hàng của NetaBooks</h5>
+        <h5 class="text-white mt-4">Gian hàng của BARNES & NOBLE</h5>
         <p><a href="#" class="text-white text-decoration-none">Trên Tiki</a></p>
         <p><a href="#" class="text-white text-decoration-none">Trên Lazada</a></p>
         <p><a href="#" class="text-white text-decoration-none">Trên Shopee</a></p>
@@ -59,7 +59,7 @@
     </div>
   </div>
   <div class="text-center p-3 text-dark" style="background-color: #F2E8C6;" >
-    © 2019 - Bản quyền của Công ty TNHH Neta Việt Nam – www.netabooks.vn
+    © 2024 - Bản quyền của Công ty TNHH Neta Việt Nam – www.barnesnoble.vn
   </div>
 </footer>
 <!-- End of Footer -->
